2a65c0f3e88b58b2dc61f1a30f00ef2286ef86e0
Gabriel Barreto <barreto-gabriel@outlook.com>
2020-03-23 20:31:55 -0300

    feat(controllers): finish subscriptions controller
    Handle with payload validation
    handle with duplicated emails
    handle with mail send

--- src/Controllers/subscription.controller.ts
+++ src/Controllers/subscription.controller.ts
@@ -1,14 +1,38 @@
+import * as Yup from 'yup'
 import { Request, Response } from 'express'
 
+import { Mailer } from '@functions'
 import { Subscription } from '@models'
 import { $response } from '@utils'
 
-const create = async (_: Request, res: Response): Promise<Response> => {
-  const found = await Subscription.find()
-  if (!found || found.length < 1) {
-    return $response.notFound(res, { error: 'Not found any document' })
+const create = async (req: Request, res: Response): Promise<Response> => {
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    email: Yup.string()
+      .email()
+      .required(),
+    phone: Yup.string().required()
+  })
+  const { body: payload } = req
+
+  const isSchemaValid = await schema.isValid(payload)
+  if (!isSchemaValid) {
+    return $response.badRequest(res, {
+      error: 'Validation fails. Invalid payload'
+    })
   }
-  return $response.success(res, found)
+
+  const found = await Subscription.findOne({ email: payload.email })
+  if (found) {
+    return $response.conflict(res, { error: 'Email already taken' })
+  }
+
+  const [created] = await Promise.all([
+    Subscription.create(payload),
+    Mailer.sentContentInfo(payload.email, { name: payload.name })
+  ])
+
+  return $response.created(res, created)
 }
 
 export default { create }
---
+++ src/__tests__/Controllers/subscriptions.spec.ts
@@ -0,0 +1,82 @@
+import faker from 'faker'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import Mongoose from 'mongoose'
+import request from 'supertest'
+
+import App from '../../app'
+
+import { Mailer } from '../../Functions'
+
+describe('Subscription Controller', () => {
+  const $http = request(App.app)
+  const basePath = '/api/subscription'
+  const mongod = new MongoMemoryServer({
+    instance: { dbName: 'jest', port: 27019 }
+  })
+
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+    await Mongoose.connect(uri, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  })
+
+  afterAll(async () => {
+    await mongod.stop()
+  })
+
+  describe('POST / - Register new subscription', () => {
+    const subPayload = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber()
+    }
+
+    it('should respond with 201 when success', async () => {
+      const { status, body } = await $http.post(basePath).send(subPayload)
+
+      expect(status).toBe(201)
+      expect(body).toEqual(expect.objectContaining(subPayload))
+    })
+
+    it('should respond with 400 when had an incomplete payload', async () => {
+      const name = faker.name.findName()
+      const phone = faker.phone.phoneNumber()
+      const email = faker.internet.email()
+
+      const noName = { email, phone }
+      const noPhone = { email, name }
+      const noEmail = { name, phone }
+
+      const payload = [noEmail, noName, noPhone][faker.random.number(2)]
+      const { status, body } = await $http.post(basePath).send(payload)
+
+      expect(status).toBe(400)
+      expect(body).toHaveProperty('error')
+    })
+
+    it('should respond with 409 when email already taken', async () => {
+      const { status, body } = await $http.post(basePath).send(subPayload)
+      expect(status).toBe(409)
+      expect(body).toHaveProperty('error')
+    })
+
+    it('should call Mailer to send email with content', async () => {
+      const mailerSpy = jest.spyOn(Mailer, 'sentContentInfo')
+      const payload = {
+        name: faker.name.findName(),
+        phone: faker.phone.phoneNumber(),
+        email: faker.internet.email()
+      }
+
+      await $http.post(basePath).send(payload)
+
+      expect(mailerSpy).toHaveBeenCalledWith(payload.email, {
+        name: payload.name
+      })
+    })
+  })
+})
